fcf26df91f6c03cd268fc53e4bae826e6d072c99
Kaleb Lee <klee.337x@gmail.com>
2026-07-23 22:29:48 -0700

    github actions

---
+++ terraform/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "kel-aws-org"
+
+        workspaces {
+            name = "grades-predictor-MLOps-practice-#{tf-env}#"
+        }
+    }
+
+    required_version = ">= 1.3"
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+        awscc = {
+            version = "0.77.0"
+        }
+    }
+}
