41878cff61ac60c7e8e5aa22ff91e779f208d2fb
Kaleb Lee <klee.337x@gmail.com>
2026-09-02 21:52:47 -0700

    Resolve 'Error: Both a backend and Terraform Cloud configuration are present'

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,20 +1,7 @@
-terraform {
-  cloud {
-    organization = "kel-aws-org"
-
-    workspaces {
-      name = "students-mlops"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region = var.aws_region
 }
 
-provider "aws" {
+provider "awscc" {
   region = var.aws_region
 }
