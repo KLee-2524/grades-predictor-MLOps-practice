1d8311dcf4d870de3a6057aff9b61e46e2712808
Kaleb Lee <klee.337x@gmail.com>
2026-09-05 15:54:40 -0700

    fix whitespace in sagemaker.tf

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -99,8 +99,8 @@ resource "aws_sagemaker_training_job" "students_training" {
   }
 
   resource_config {
-    instance_type  = var.training_instance_type
-    instance_count = 1
+    instance_type     = var.training_instance_type
+    instance_count    = 1
     volume_size_in_gb = 10
   }
 
