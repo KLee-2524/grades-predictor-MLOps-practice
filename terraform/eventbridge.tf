06b3194991c26e63ffd6f7ca5ae960d05cd20ed9
Kaleb Lee <klee.337x@gmail.com>
2026-09-05 13:14:39 -0700

    merge eventbridge_trigger.tf and eventbridge_monitoring.tf into a single eventbridge.tf file and remove unused outputs.tf

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,3 +1,6 @@
+################################
+# Eventbridge Trigger
+################################
 resource "aws_cloudwatch_event_rule" "new_csv_trigger" {
   name        = "${var.resource_name_prefix}-ev-new-csv"
   description = "Trigger pipeline when new CSV arrives"
@@ -21,4 +24,20 @@ resource "aws_cloudwatch_event_target" "pipeline_target" {
   rule     = aws_cloudwatch_event_rule.new_csv_trigger.name
   arn      = aws_sagemaker_pipeline.students_pipeline.arn
   role_arn = aws_iam_role.eventbridge_sagemaker.arn
+}
+
+################################
+# Eventbridge Monitoring
+################################
+resource "aws_cloudwatch_event_rule" "monitoring_alerts" {
+  name = "${var.resource_name_prefix}-ev-monitoring-alerts"
+  event_pattern = jsonencode({
+    "source" : ["aws.sagemaker"],
+    "detail-type" : ["SageMaker Model Monitor Alert"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "monitoring_target" {
+  rule = aws_cloudwatch_event_rule.monitoring_alerts.name
+  arn  = aws_sns_topic.alerts.arn
 }
