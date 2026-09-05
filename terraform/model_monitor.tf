5a9148bf1307e5fc6c39e301f773673b72b0add5
Kaleb Lee <klee.337x@gmail.com>
2026-09-05 15:01:56 -0700

    Fix spelling error in terraform/model_monitor.tf

--- terraform/model_monitor.tf
+++ terraform/model_monitor.tf
@@ -10,7 +10,7 @@ resource "aws_sagemaker_monitoring_schedule" "data_quality" {
     #}
 
     monitoring_job_definition {
-      monitoring_app_specifications {
+      monitoring_app_specification {
         image_uri = "${var.monitor_image_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/sagemaker-model-monitor-analyzer"
       }
 
